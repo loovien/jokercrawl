3e595fc2071b39b10f62892a9d23f007d4e5c685
luowen <bigpao.luo@gmail.com>
2017-12-18 23:33:50 +0800

    add time parse demo

--- jokers/spiders/test.py
+++ jokers/spiders/test.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
-import time
+
+import datetime
 
 
-time.time()
+def time_parse():
+    now = datetime.datetime.now()
+    str_time = now.strftime("%m-%d %H:%M")
+    print(">>>>> parse time:")
+    with_year = "{year}-{suffix}".format(year="2017", suffix=str_time)
+    print(with_year)
+    new_time = datetime.datetime.strptime(with_year, "%Y-%m-%d %H:%M")
+    print(">>>>> string to time:")
+    print(new_time.timestamp())
 
+
+if __name__ == '__main__':
+    time_parse()
