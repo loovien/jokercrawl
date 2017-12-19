3f68b0f52eb3629d58f989738ad50561d40d1133
luowen <bigpao.luo@gmail.com>
2017-12-20 00:38:25 +0800

    pre deploy

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# author: luowen<bigpao.luo@gmail.com>
+# time: 2017/12/20 0:34
+# desc: distutils pakcage deploy
+
+from distutils.core import setup
+
+"""
+distclass,
+script_name, 
+script_args, 
+options,
+name,
+version,
+author,
+author_email,
+maintainer,
+maintainer_email,
+url,
+license,
+description,
+long_description,
+keywords,
+platforms,
+classifiers,
+download_url,
+requires,
+provides,
+obsoletes,
+"""
+
+
